fbf6feaf6d12f4504e9ae133ae9cbc9237a3602c
farhnalwnd <raja.farhan.alwanda@gmail.com>
2026-02-06 09:10:50 +0700

    fix: create trait for direct create and edit method to index

--- app/Filament/Resources/ApprovalMasters/Pages/EditApprovalMaster.php
+++ app/Filament/Resources/ApprovalMasters/Pages/EditApprovalMaster.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Resources\ApprovalMasters\Pages;
 
 use App\Filament\Resources\ApprovalMasters\ApprovalMasterResource;
+use App\Traits\indexDirect;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditApprovalMaster extends EditRecord
 {
+    use indexDirect;
+
     protected static string $resource = ApprovalMasterResource::class;
 
     protected function getHeaderActions(): array
--- app/Filament/Resources/MenuMgts/Pages/EditMenuMgt.php
+++ app/Filament/Resources/MenuMgts/Pages/EditMenuMgt.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Resources\MenuMgts\Pages;
 
 use App\Filament\Resources\MenuMgts\MenuMgtResource;
+use App\Traits\indexDirect;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditMenuMgt extends EditRecord
 {
+    use indexDirect;
+
     protected static string $resource = MenuMgtResource::class;
 
     protected function getHeaderActions(): array
