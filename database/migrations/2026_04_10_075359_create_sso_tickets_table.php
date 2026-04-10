ef6421ad39f16683d844d5cbc217f2c695faaa46
farhnalwnd <raja.farhan.alwanda@gmail.com>
2026-04-10 15:06:56 +0700

    chor: add table sso_tickets

--- database/migrations/2026_04_10_075359_create_sso_tickets_table.php
+++ database/migrations/2026_04_10_075359_create_sso_tickets_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sso_tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('portal_application.users');
+            $table->string('ticket');
+            $table->timestamp('expired_at');
             $table->timestamps();
         });
     }
