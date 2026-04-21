b76eecc9f93b0ba85f23588644de23a90fd6d68d
farhnalwnd <raja.farhan.alwanda@gmail.com>
2026-04-21 09:25:34 +0700

    fix: ui admin dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,10 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->viteTheme('resources/css/filament/admin/theme.css')
             // ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Cyan[600],
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -55,6 +56,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->plugins([]);
+            ->plugins([])
+            ->viteTheme('resources/css/filament/admin/theme.css');
     }
 }
